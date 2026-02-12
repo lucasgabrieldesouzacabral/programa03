b3b195dcf6c2483d1c9003b9f56876a42dda25cf
lucasgabrieldesouzacabral <lucasdesouza616@gmail.com>
2026-02-12 08:43:13 -0300

    OK

--- App.js
+++ App.js
@@ -10,9 +10,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#274360',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 40,
   },
-  
+
   logo: {
     width: 140,
     height: 140,
@@ -23,14 +24,14 @@ const styles = StyleSheet.create({
   label: {
     color: 'white',
     alignSelf: 'flex-start',
-    marginLeft: 25,
+    marginLeft: -80,
     marginTop: 15,
     marginBottom: 5,
     fontSize: 16,
   },
 
   input: {
-    width: '85%',
+    width: '300%',
     height: 45,
     backgroundColor: '#dcdcdc',
     borderRadius: 8,
@@ -38,7 +39,7 @@ const styles = StyleSheet.create({
   },
 
   botao: {
-    width: '85%',
+    width: '300%',
     height: 50,
     backgroundColor: '#3CB371',
     borderRadius: 12,
@@ -58,9 +59,32 @@ const styles = StyleSheet.create({
     marginTop: 15,
     textDecorationLine: 'underline',
   },
+
+  h1: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    Color:'white',
+  },
+
+  header: {
+    width: '85%',
+    paddingVertical: 12,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    alignItems: 'center',
+  },
+
+  content: {
+    width: '100%',
+    padding: 150,
+    borderBottomLeftRadius: 200,
+    borderBottomRightRadius: 200,
+    alignItems: 'center',
+  },
 });
 
-export default function App() {
+export default function Login() {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const handleLogin = () => {
@@ -71,12 +95,14 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
 
+        <View style={styles.content}>
         <Image
           style={styles.logo}
           source={{
             uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFTLo7PcZq1YtJNfZEobioBzdrK9YxMJ0PRA&s',
           }}
         />
+
         <Text style={styles.label}>Email</Text>
         <TextInput
           style={styles.input}
@@ -103,6 +129,7 @@ export default function App() {
         </TouchableOpacity>
 
         <Text style={styles.esqueceu}>esqueceu a senha</Text>
+        </View>
 
         <StatusBar style="auto" />
       </SafeAreaView>
@@ -110,6 +137,95 @@ export default function App() {
   );
 }
 
+function Cadastro() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const handleLogin = () => {
+    console.log("Nome:", nome);
+    console.log("Email:", email);
+    console.log("Senha:", senha);
+  };
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+        <Text
+          style={styles.h1}
+          h1
+        >
+          Cadastro
+        </Text>
+        </View>
 
+        <View style={styles.content}>
+        <Text style={styles.label}>Nome</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu Nome"
+          value={nome}
+          onChangeText={setNome}
+        />
 
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu email"
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <Text style={styles.label}>Senha</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua senha"
+          value={senha}
+          onChangeText={setSenha}
+          secureTextEntry={true}
+        />
+
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.textoBotao}>Cadastrar</Text>
+        </TouchableOpacity>
+        </View>
+
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+function esqueceu() {
+  const [email, setEmail] = useState('');
+  const handleLogin = () => {
+    console.log("Email:", email);
+  };
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+
+        <View style={styles.header}>
+          <Text style={styles.h1}>Esqueceu a senha</Text>
+        </View>
+
+        <View style={styles.content}>
+          <Text style={styles.label}>Email</Text>
+
+          <TextInput
+            style={styles.input}
+            placeholder="Digite seu email"
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <TouchableOpacity style={styles.botao} onPress={handleLogin}>
+            <Text style={styles.textoBotao}>Enviar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
 
