a06d0038fbc393fe40fa5d6c7b0c95f986299cf6
lucasgabrieldesouzacabral <lgsc5@discente.ifpe.edu.br>
2026-02-25 19:56:52 -0300

    ultima aula como base

--- App.js
+++ App.js
@@ -4,228 +4,69 @@ import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useRoute } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+
+
+function HomeScreen({route}) {
+const { username, idade } = route.params;
+return (
+<View style={{ flex: 1, alignItems: 'center'
+, justifyContent: 'center' }}>
+<Text>Bem-vindo, {username}!</Text>
+<Text>Idade: {idade}</Text>
+</View>
+);
+}
+
+function LoginScreen({ navigation }) {
+return (
+<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+<Text>Login Screen</Text>
+<Button title='Home' onPress={() => navigation.navigate('HomeScreen', { username: 'Nilson',idade: 30 })}>
+  <Button
+  onPress={onPressLearnMore}
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"/>
+</Button>
+</View>
+);
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+return (
+<NavigationContainer>
+<Stack.Navigator initialRouteName="Login">
+<Stack.Screen name="Home" component={HomeScreen}
+options={{headerTitleAlign: 'center'
+, }} />
+<Stack.Screen name="Login" component={LoginScreen}
+options={{ headerShown: false, 
+  headerRight: () => (
+    <Ionicons
+    name="notifications-outline"
+    size={24}
+    color="black"
+    style={{ marginRight: 10 }}
+    />
+  )
+}} />
+</Stack.Navigator>
+</NavigationContainer>
+);
+}
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#274360',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingTop: 40,
-  },
-
-  logo: {
-    width: 140,
-    height: 140,
-    borderRadius: 70,
-    marginBottom: 20,
-  },
-
-  label: {
-    color: 'white',
-    alignSelf: 'flex-start',
-    marginLeft: -80,
-    marginTop: 15,
-    marginBottom: 5,
-    fontSize: 16,
-  },
-
-  input: {
-    width: '300%',
-    height: 45,
-    backgroundColor: '#dcdcdc',
-    borderRadius: 8,
-    paddingHorizontal: 10,
-  },
-
-  botao: {
-    width: '300%',
-    height: 50,
-    backgroundColor: '#3CB371',
-    borderRadius: 12,
     justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 15,
-  },
-
-  textoBotao: {
-    color: 'white',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-
-  esqueceu: {
-    color: 'white',
-    marginTop: 15,
-    textDecorationLine: 'underline',
-  },
-
-  h1: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    Color:'white',
-  },
-
-  header: {
-    width: '85%',
-    paddingVertical: 12,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    alignItems: 'center',
-  },
-
-  content: {
-    width: '100%',
-    padding: 150,
-    borderBottomLeftRadius: 200,
-    borderBottomRightRadius: 200,
-    alignItems: 'center',
   },
 });
-
-export default function Login() {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const handleLogin = () => {
-    console.log("Email:", email);
-    console.log("Senha:", senha);
-  };
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-
-        <View style={styles.content}>
-        <Image
-          style={styles.logo}
-          source={{
-            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFTLo7PcZq1YtJNfZEobioBzdrK9YxMJ0PRA&s',
-          }}
-        />
-
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite seu email"
-          value={email}
-          onChangeText={setEmail}
-        />
-
-        <Text style={styles.label}>Senha</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite sua senha"
-          value={senha}
-          onChangeText={setSenha}
-          secureTextEntry={true}
-        />
-
-        <TouchableOpacity style={styles.botao} onPress={handleLogin}>
-          <Text style={styles.textoBotao}>Logar</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.botao}>
-          <Text style={styles.textoBotao}>Cadastre-se</Text>
-        </TouchableOpacity>
-
-        <Text style={styles.esqueceu}>esqueceu a senha</Text>
-        </View>
-
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
-}
-
-function Cadastro() {
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const handleLogin = () => {
-    console.log("Nome:", nome);
-    console.log("Email:", email);
-    console.log("Senha:", senha);
-  };
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-        <Text
-          style={styles.h1}
-          h1
-        >
-          Cadastro
-        </Text>
-        </View>
-
-        <View style={styles.content}>
-        <Text style={styles.label}>Nome</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite seu Nome"
-          value={nome}
-          onChangeText={setNome}
-        />
-
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite seu email"
-          value={email}
-          onChangeText={setEmail}
-        />
-
-        <Text style={styles.label}>Senha</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite sua senha"
-          value={senha}
-          onChangeText={setSenha}
-          secureTextEntry={true}
-        />
-
-        <TouchableOpacity style={styles.botao}>
-          <Text style={styles.textoBotao}>Cadastrar</Text>
-        </TouchableOpacity>
-        </View>
-
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
-}
-
-function esqueceu() {
-  const [email, setEmail] = useState('');
-  const handleLogin = () => {
-    console.log("Email:", email);
-  };
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-
-        <View style={styles.header}>
-          <Text style={styles.h1}>Esqueceu a senha</Text>
-        </View>
-
-        <View style={styles.content}>
-          <Text style={styles.label}>Email</Text>
-
-          <TextInput
-            style={styles.input}
-            placeholder="Digite seu email"
-            value={email}
-            onChangeText={setEmail}
-          />
-
-          <TouchableOpacity style={styles.botao} onPress={handleLogin}>
-            <Text style={styles.textoBotao}>Enviar</Text>
-          </TouchableOpacity>
-        </View>
-
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
-}
-
